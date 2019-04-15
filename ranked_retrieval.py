663d7aa6d0fa89811b94ebf295ab772855c0ca5d
pengnam <seanngpengnam@gmail.com>
2019-04-15 16:28:05 +0800

    Resolve load_document_vector

--- ranked_retrieval.py
+++ ranked_retrieval.py
@@ -1,7 +1,7 @@
 from typing import Tuple, List, Dict, BinaryIO
 from data_structures import LinkedList
 from collections import Counter, defaultdict
-from search_helpers import normalise, load_postings_list
+from search_helpers import normalise, load_postings_list, load_document_vector
 
 ALPHA = 1.0
 BETA = 0.75
@@ -21,13 +21,13 @@ def get_relevant_docs(
                                 relevant_doc_ids,
                                 document_vectors_dictionary,
                                 ALPHA,
-                                BETA)
+                                BETA, postings_file)
 
     scores = defaultdict(float)
     for term, count in newQuery.items():
         if term not in dictionary:
             continue
-        term_postings = load_postings_list(postings_file, dictionary, term)
+        term_postings = Counter(load_postings_list(postings_file, dictionary, term))
         idf = dictionary[term][0]
         for doc_id, tf_d in term_postings:
             scores[doc_id] += tf_d * idf
@@ -37,7 +37,7 @@ def get_relevant_docs(
                                reverse=True)
     normalized_scores = [*filter(lambda x: x[1] > THRESHOLD, normalized_scores)]
     output = LinkedList()
-    output.extend(normalized_scores)
+    output.extend(map(lambda x:x[0], normalized_scores))
     return output
 
 
@@ -50,10 +50,12 @@ def rocchio_algorithm(
         relevant_doc_ids: List[str],
         docs_vector: Dict[str, Tuple[int, int]],
         alpha: int,
-        beta: int) -> Dict[str, int]:
-    relevant_docs_sum = sum([Counter(docs_vector[doc_id]) 
-                            for doc_id in relevant_doc_ids], Counter())
-    relevant_docs_centroid = {doc_id: beta * count / len(relevant_doc_ids)
-                              for doc_id, count in relevant_docs_sum.items()}
-    normalized_query = {k: alpha * v for k, v in query.items()}
-    return dict(Counter(normalized_query) + Counter(relevant_docs_centroid))
+        beta: int, postings_file: BinaryIO) -> Dict[str, int]:
+    relevant_docs_sum = Counter()
+    for doc_id in relevant_doc_ids:
+        relevant_docs_sum += load_document_vector(doc_id, postings_file, docs_vector)
+    relevant_docs_centroid = Counter({doc_id: beta*count / len(relevant_doc_ids)
+                              for doc_id, count in relevant_docs_sum.items()})
+
+    normalized_query = Counter({k: alpha * v for k, v in query.items()})
+    return dict(normalized_query + relevant_docs_centroid)
